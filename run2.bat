e5bfac6e91d75226fd9b492fbda14269aacf22cc
Piotr Józefiak <pjozefiak32@gmail.com>
2023-06-06 17:22:31 +0200

    add filename setting

--- run2.bat
+++ run2.bat
@@ -1,5 +1,11 @@
 @echo off
 
-set /p "link=Podaj link do filmu: "
+setlocal enabledelayedexpansion
 
-docker run -it --rm -v "%cd%":/app youtube-subtitles yt-dlp -f mp4 "%link%"
+set /p "link=Podaj link do filmu: "
+for /f "tokens=2 delims==" %%i in ("%link%") do (
+    set "datestamp=!date:~0,2!-!date:~3,2!-!date:~8,2!"
+    set "filename=!datestamp!_%%i.mp4"
+)
+
+docker run -it --rm -v "%cd%":/app youtube-subtitles yt-dlp -o "/app/!filename!" -f mp4 "%link%"
